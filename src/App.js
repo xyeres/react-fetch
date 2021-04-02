4d5449f815c50043203e4af83e06f9484c7cfd3a
Michael Carr <xyeres@me.com>
2021-04-02 15:34:54 -0700

    finished project

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+import axios from 'axios';
+
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      gifs: [],
+      loading: true
+    }
+  } 
+
+  componentDidMount() {
+    this.performSearch();
+
+    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    //   .then(res => res.json())
+    //   .then(resData => {
+    //     this.setState({gifs: resData.data});
+    //   })
+    //   .catch(err => {
+    //     console.error('Errer fetching data ', err);
+    //   })
+  }
+
+  performSearch = (query = 'gurren lagen') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=25&api_key=dc6zaTOxFJmzC`)
+    .then(res => {
+        this.setState({
+          gifs: res.data.data,
+          loading: false
+        })
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
+  render() { 
+
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm onSearch={this.performSearch} />      
+          </div>   
+        </div>    
+        <div className="main-content">
+        {
+          (this.state.loading)
+          ? <p>Loading...</p>
+          : <GifList data={this.state.gifs} />
+        }
+        </div>
+      </div>
+    );
+  }
+}
