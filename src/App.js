eb34c3cbfd02e1b3c044b6b398318d8138fc0566
Michael Carr <xyeres@me.com>
2021-04-02 16:00:52 -0700

    secure api call

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default class App extends Component {
   }
 
   performSearch = (query = 'gurren lagen') => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=25&api_key=dc6zaTOxFJmzC`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=25&api_key=dc6zaTOxFJmzC`)
     .then(res => {
         this.setState({
           gifs: res.data.data,
